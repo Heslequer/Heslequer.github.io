c2a569387dc5da851cb6c942ba434ab680c10fc2
Heslequer <141370642+Heslequer@users.noreply.github.com>
2024-02-01 21:55:31 -0300

    Add files via upload

--- jsPRetaf.js
+++ jsPRetaf.js
@@ -44,36 +44,47 @@ fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTq9cuxu_qIW0JLyUAs1EKwBD
 $(document).ready(function() {
     $('#nome').select2();
 });
+function gerarData()
+{
+  var d = new Date();
+  var dia = String(d.getDate()).padStart(2,'0');
+  var mes = String(d.getMonth()+1).padStart(2,'0');
+  var ano = String(d.getFullYear()).substr(-2)
+  var hora = String(d.getHours())
+  var min = String(d.getMinutes())
+  var sec = String(d.getSeconds())
+  var data = dia +'/' + mes + '/' + ano + ' ' + hora + ':' + min + ':' + sec
+  return data
+
+}
 document.addEventListener('DOMContentLoaded', function() {
 
     const clicou = (event) => {
         event.preventDefault();
-        var valorNome = document.getElementById("nome").value
-        if(valorNome === 'Selecione o Nome'){
-            valorNome = document.getElementById('nomeCompleto').value
-
+        var nome = document.getElementById("nome").value
+        if(nome === 'Selecione o Nome'){
+            nome = document.getElementById('nomeCompleto').value
         }
-        const postoDeGraduacao = document.querySelector('#pg').value
+        const postodeGraduacao = document.querySelector('#pg').value
         const organizacaoMilitar = document.querySelector('#om').value
         const subunidade = document.querySelector('#sb').value
-        const valorIdade = document.querySelector('#idade').value
-        const valorPeso = document.querySelector('#peso').value
-        const valorAltura = document.querySelector('#altura').value
-        const valorCintura = document.querySelector('#cintura').value
-        const valorAbdomen = document.querySelector('#abdomen').value
-        const valorQuadril = document.querySelector('#quadril').value
-        const valorPressao = document.querySelector('#pressao').value
-        const valorParecer = document.querySelector('#parecer').value
-        console.log('foi '+valorNome)
-        console.log('foi '+valorIdade)
-
+        const idade = document.querySelector('#idade').value
+        const peso = document.querySelector('#peso').value
+        const altura = document.querySelector('#altura').value
+        const cintura = document.querySelector('#cintura').value
+        const abdomen = document.querySelector('#abdomen').value
+        const quadril = document.querySelector('#quadril').value
+        const pressao = document.querySelector('#pressao').value
+        const parecerOdonto = document.querySelector('#parecerOdt').value
+        const parecerMedico = document.querySelector('#parecer').value
+        const data = gerarData()
         fetch('https://api.sheetmonkey.io/form/gWNF9GS36ff4Bqrido88dK', {
         method: 'post',
         headers : {
             'Accept': 'application/json',
             'Content-Type': 'application/json', 
         },
-        body: JSON.stringify({ valorNome , valorIdade, postoDeGraduacao, organizacaoMilitar,subunidade,valorPeso, valorAltura, valorCintura,valorAbdomen,valorQuadril,valorPressao,valorParecer })
+        body: JSON.stringify({ nome , idade, postodeGraduacao, organizacaoMilitar,subunidade,peso, altura, cintura,abdomen,quadril,pressao,parecerOdonto,parecerMedico, data })
         });
         
          alert('ENVIADO COM SUCESSO')
